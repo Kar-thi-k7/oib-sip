1d791ee64671392e6acddc81f2667ddb30debb5e
Kar-thi-k7 <dharmanakarthik24@gmail.com>
2024-05-08 18:22:37 +0530

    Add files via upload

---
+++ passwordGen.py
@@ -0,0 +1,35 @@
+import string
+import random
+import secrets
+
+def generate_pasword(length, use_letters = True, use_numbers = True, use_symbols=True):
+    selection_list = ''
+    if use_letters:
+        selection_list += string.ascii_letters
+
+    if use_numbers:
+        selection_list += string.digits
+
+    if use_symbols:
+        selection_list += string.punctuation
+
+    if not selection_list:
+        print("Please select st least one character type.")
+        return None
+    
+    password = ''.join(secrets.choice(selection_list) for _ in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the lenght of your password: "))
+    use_letters=input("Include letters? (yes/no): ").lower() == 'yes'
+    use_numbers=input("Include numbers? (yes/no): ").lower() == 'yes'
+    use_symbols=input("Include symbols? (yes/no): ").lower() == 'yes'
+
+    password = generate_pasword(length, use_letters, use_numbers, use_symbols)
+
+    if password:
+        print("Generate Password:", password)
+    else:
+        print("Password generation failed. Please try again")
+main()
